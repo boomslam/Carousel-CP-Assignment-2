82eec67cc67ca021c50dedb0db075534d7328564
Jess <jesswlam@gmail.com>
2016-02-09 20:08:44 -0800

    Initial Commit

---
+++ Carousel CP Assignment 2Tests/Carousel_CP_Assignment_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Carousel_CP_Assignment_2Tests.swift
+//  Carousel CP Assignment 2Tests
+//
+//  Created by Jess Lam on 2/9/16.
+//  Copyright © 2016 Jess Lam. All rights reserved.
+//
+
+import XCTest
+@testable import Carousel_CP_Assignment_2
+
+class Carousel_CP_Assignment_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
