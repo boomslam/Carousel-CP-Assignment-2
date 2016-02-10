b6b7ac39d8129e426814f294300c3d0c1f64e1c2
Jess <jesswlam@gmail.com>
2016-02-10 09:28:13 -0800

    Configured app icon, launch image, intro screen view controller with scroll view, and login screen with scroll view.

---
+++ Carousel CP Assignment 2/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Carousel CP Assignment 2
+//
+//  Created by Jess Lam on 2/10/16.
+//  Copyright © 2016 Jess Lam. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
