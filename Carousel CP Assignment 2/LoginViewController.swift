216bf63b2e9d99ea9baa3039a28c70bcc7849208
Jess <jesswlam@gmail.com>
2016-02-11 00:08:45 -0800

    Registered keyboard events and offset view position when the keyboard is shown.

--- Carousel CP Assignment 2/LoginViewController.swift
+++ Carousel CP Assignment 2/LoginViewController.swift
@@ -8,12 +8,50 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UIScrollViewDelegate {
 
+    func keyboardWillShow(notification: NSNotification!) {
+        fieldParentView.frame.origin.y = initialY + offset
+        print("keyboardWillShow")
+        // Move the button up above keyboard
+        buttonParentView.frame.origin.y = buttonInitialY + buttonOffset
+        // Scroll the scrollview up
+        loginScrollView.contentOffset.y = loginScrollView.contentInset.bottom
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        fieldParentView.frame.origin.y = initialY
+        
+    }
+    
+    @IBOutlet weak var fieldParentView: UIView!
     @IBOutlet weak var loginScrollView: UIScrollView!
+    @IBOutlet weak var buttonParentView: UIView!
+    
+    var initialY: CGFloat!
+    var offset: CGFloat!
+    
+    var buttonInitialY: CGFloat!
+    var buttonOffset: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        initialY = fieldParentView.frame.origin.y
+        offset = -50
+        
+        buttonInitialY = buttonParentView.frame.origin.y
+        buttonOffset = -80
+        
+        loginScrollView.contentSize = loginScrollView.frame.size
+        loginScrollView.contentInset.bottom = 100
+        
+        loginScrollView.delegate = self
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +60,9 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
