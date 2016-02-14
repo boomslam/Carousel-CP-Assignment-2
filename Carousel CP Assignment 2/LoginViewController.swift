eb4c98386a9c24aa10568609d58123d2ec73919a
Jess <jesswlam@gmail.com>
2016-02-13 16:10:41 -0800

    Added invalid login credentials alert.

--- Carousel CP Assignment 2/LoginViewController.swift
+++ Carousel CP Assignment 2/LoginViewController.swift
@@ -9,46 +9,34 @@
 import UIKit
 
 class LoginViewController: UIViewController, UIScrollViewDelegate {
-
-    func keyboardWillShow(notification: NSNotification!) {
-        fieldParentView.frame.origin.y = initialY + offset
-        print("keyboardWillShow")
-        // Move the button up above keyboard
-        buttonParentView.frame.origin.y = buttonInitialY + buttonOffset
-        // Scroll the scrollview up
-        loginScrollView.contentOffset.y = loginScrollView.contentInset.bottom
-        
-    }
-    
-    func keyboardWillHide(notification: NSNotification!) {
-        fieldParentView.frame.origin.y = initialY
-         buttonParentView.frame.origin.y = buttonInitialY
-    }
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var loginIndicator: UIActivityIndicatorView!
-    
-    
     @IBOutlet weak var fieldParentView: UIView!
     @IBOutlet weak var loginScrollView: UIScrollView!
     @IBOutlet weak var buttonParentView: UIView!
-    
-    var initialY: CGFloat!
-    var offset: CGFloat!
+    @IBOutlet weak var loginNavBar: UIImageView!
+  
     
     var buttonInitialY: CGFloat!
     var buttonOffset: CGFloat!
     
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        initialY = fieldParentView.frame.origin.y
-        offset = -50
-        
         buttonInitialY = buttonParentView.frame.origin.y
-        buttonOffset = -80
+        buttonOffset = -5
         
         loginScrollView.contentSize = loginScrollView.frame.size
         loginScrollView.contentInset.bottom = 100
@@ -68,53 +56,84 @@ class LoginViewController: UIViewController, UIScrollViewDelegate {
     }
     
 
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        // If the scrollView has been scrolled down by 50px or more...
-        if scrollView.contentOffset.y <= -50 {
-            // Hide the keyboard
-            view.endEditing(true)
-        }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // Set initial transform values 20% of original size
+        let transform = CGAffineTransformMakeScale(0.2, 0.2)
+        // Apply the transform properties of the views
+        loginNavBar.transform = transform
+        fieldParentView.transform = transform
+        // Set the alpha properties of the views to transparent
+        loginNavBar.alpha = 0
+        fieldParentView.alpha = 0
     }
     
-    @IBAction func didTap(sender: AnyObject) {
-        view.endEditing(true)
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        //Animate the code within over 0.3 seconds...
+        UIView.animateWithDuration(0.3) { () -> Void in
+            // Return the views transform properties to their default states.
+            self.fieldParentView.transform = CGAffineTransformIdentity
+            self.loginNavBar.transform = CGAffineTransformIdentity
+            // Set the alpha properties of the views to fully opaque
+            self.fieldParentView.alpha = 1
+            self.loginNavBar.alpha = 1
+        }
     }
     
     
     @IBAction func didPressLogin(sender: AnyObject) {
         loginIndicator.startAnimating()
-        loginButton.selected = true
-        if emailField.text == "q" && passwordField.text == "q" {
-            delay(2, closure: { () -> () in
-                self.loginIndicator.stopAnimating()
-                self.loginButton.selected = false
-                self.performSegueWithIdentifier("firstSegue", sender: nil)
-            })
-        } else {
-            delay(2, closure: { () -> () in
-                self.loginIndicator.stopAnimating()
-                self.loginButton.selected = false
-            })
-            let alertController = UIAlertController(title: "Error", message: "Email and password do not match.", preferredStyle: .Alert)
-            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+        
+        if emailField.text!.isEmpty || passwordField.text!.isEmpty{
+            
+            let alertController  = UIAlertController(title: "Email Required", message: "Please try again.", preferredStyle: .Alert)
+            
+            let cancelAction = UIAlertAction(title: "OK", style: .Cancel) { (action) in
             }
             alertController.addAction(cancelAction)
-            presentViewController(alertController, animated: true) {
+            presentViewController(alertController, animated: true){
             }
+            self.loginIndicator.stopAnimating()
+        }else if emailField.text == "q" && passwordField.text == "q"{
+            delay(2, closure: { () -> () in
+                self.loginIndicator.stopAnimating()
+                self.performSegueWithIdentifier("firstSegue", sender: nil)
+                
+            })
+        }else{
+            delay(2, closure: { () -> () in
+                self.loginIndicator.stopAnimating()
+                let failController  = UIAlertController(title: "Incorrect Email and Password", message: "Please try again.", preferredStyle: .Alert)
+                
+                let cancelAction = UIAlertAction(title: "OK", style: .Cancel) { (action) in
+                }
+                failController.addAction(cancelAction)
+                self.presentViewController(failController, animated: true){
+                }
+                
+                
+            })
+            
         }
     }
     
-    @IBAction func onButton(sender: AnyObject) {
-        self.loginIndicator.startAnimating()
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        buttonParentView.frame.origin.y = buttonInitialY + buttonOffset
+        loginScrollView.contentOffset.y = loginScrollView.contentInset.bottom
+        
     }
     
-    func delay(delay:Double, closure:()->()) {
-        dispatch_after(
-            dispatch_time(
-                DISPATCH_TIME_NOW,
-                Int64(delay * Double(NSEC_PER_SEC))
-            ),
-            dispatch_get_main_queue(), closure)
+    func keyboardWillHide(notification: NSNotification!) {
+        buttonParentView.frame.origin.y = buttonInitialY
+    }
+    
+    func loginScrollViewDidScroll(scrollView: UIScrollView) {
+        if loginScrollView.contentOffset.y <= -50{
+            view.endEditing(true)
+            
+        }
     }
     
 
